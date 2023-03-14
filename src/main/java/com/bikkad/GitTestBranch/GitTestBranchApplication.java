5add886c5a75cb92ce4664d7769a302dd9981128
cdbhosle07 <cdbhosle07@gmail.com>
2023-03-14 14:04:33 +0530

    changes in project 2nd time

--- src/main/java/com/bikkad/GitTestBranch/GitTestBranchApplication.java
+++ src/main/java/com/bikkad/GitTestBranch/GitTestBranchApplication.java
@@ -3,17 +3,25 @@ package com.bikkad.GitTestBranch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class GitTestBranchApplication {
 
-	
+	@PostMapping("/purchase")
 	public String purchase(@PathVariable String userName,@PathVariable double  amount,
 			@PathVariable String productName)
 	{
 		return "HI" +userName + "order for" +productName+ "with amount" +amount + "stored successfully..!";
+	}
+	
+	public String order(@PathVariable String userName,@PathVariable double  amount)
+	{
+		return "HI" +userName + "with amount" +amount ;
+	
+		
 	}
 	
 	
